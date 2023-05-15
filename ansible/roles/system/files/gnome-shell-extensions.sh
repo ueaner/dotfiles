85e1ad2892f4d27afc5bbef4507ad3f45383f5ef
ueaner <ueaner@gmail.com>
2023-05-15 19:40:21 +0800

    chore(ansible): install gnome shell extensions

---
+++ ansible/roles/system/files/gnome-shell-extensions.sh
@@ -0,0 +1,14 @@
+python3 -m pip install -i http://mirrors.aliyun.com/pypi/simple --user gnome-extensions-cli
+
+# NOTE: Need to run locally, click `Install` to confirm the extension installation
+
+# https://extensions.gnome.org/extension/4245/gesture-improvements/
+~/.local/bin/gext install gestureImprovements@gestures
+# https://extensions.gnome.org/extension/261/kimpanel/
+~/.local/bin/gext install kimpanel@kde.org
+# https://extensions.gnome.org/extension/3724/net-speed-simplified/
+~/.local/bin/gext install netspeedsimplified@prateekmedia.extension
+
+# gesture improvements preferences
+cp "$HOME/ansible/roles/system/files/gesture-improvements.xml" \
+    "$HOME/.local/share/gnome-shell/extensions/gestureImprovements@gestures/schemas/org.gnome.shell.extensions.gestureImprovements.gschema.xml"
