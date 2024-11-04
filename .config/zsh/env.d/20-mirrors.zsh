b8ac8a64689d2d7fcfc9056a441362306c6eea91
ueaner <ueaner@gmail.com>
2024-11-04 19:05:35 +0800

    chroe: update mirrors

--- .config/zsh/env.d/20-mirrors.zsh
+++ .config/zsh/env.d/20-mirrors.zsh
@@ -1,4 +1,4 @@
-# 环境变量及源地址
+# 源地址
 
 #export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
 # 下载安装包的源
@@ -10,27 +10,20 @@ export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebr
 export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
 export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
 
-# yarn 使用 ~/.yarnrc.yml 来管理
-# https://liuwenzhuang.github.io/2020/08/07/Yarn2-install-and-usage.html
-# export YARN_ENABLE_GLOBAL_CACHE=true
-# export YARN_GLOBAL_FOLDER=~/.cache/yarn
-# export YARN_YARN_PATH=$YARN_GLOBAL_FOLDER/yarn-3.1.0.cjs
-# export YARN_NPM_REGISTRY_SERVER=https://registry.npmmirror.com
-
-# rustup
-export RUSTUP_DIST_SERVER="https://mirrors.ustc.edu.cn/rust-static"
-export RUSTUP_UPDATE_ROOT="https://mirrors.ustc.edu.cn/rust-static/rustup"
-
-# flutter
-#export PUB_HOSTED_URL=https://pub.flutter-io.cn
-#export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
-#export PUB_HOSTED_URL=https://dart-pub.mirrors.sjtug.sjtu.edu.cn/
-#export FLUTTER_STORAGE_BASE_URL=https://mirrors.sjtug.sjtu.edu.cn/
-export FLUTTER_STORAGE_BASE_URL=https://mirrors.tuna.tsinghua.edu.cn/flutter
-export PUB_HOSTED_URL=https://mirrors.tuna.tsinghua.edu.cn/dart-pub
-#export FLUTTER_STORAGE_BASE_URL="https://storage.flutter-io.cn"
-#export PUB_HOSTED_URL="https://pub.flutter-io.cn"
+# https://mirrors.tuna.tsinghua.edu.cn/help/rustup/
+# rustup self update
+# rustup install stable
+export RUSTUP_UPDATE_ROOT=https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
+# export RUSTUP_UPDATE_ROOT=https://mirrors.aliyun.com/rustup
+# cargo install ...
+# export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup
+# https://mirrors.sjtug.sjtu.edu.cn/docs/rust-static
+# export RUSTUP_DIST_SERVER=https://mirrors.sjtug.sjtu.edu.cn/rust-static/
+# https://mirrors.ustc.edu.cn/help/rust-static.html
+export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
 
 # node fnm
-#export FNM_NODE_DIST_MIRROR=https://registry.npmmirror.com/dist
-export FNM_NODE_DIST_MIRROR=https://mirrors.tuna.tsinghua.edu.cn/nodejs-release/
+# export FNM_NODE_DIST_MIRROR=https://registry.npmmirror.com/dist
+# export FNM_NODE_DIST_MIRROR=https://mirrors.tuna.tsinghua.edu.cn/nodejs-release/
+# export FNM_NODE_DIST_MIRROR=https://mirrors.aliyun.com/nodejs-release/
+export FNM_NODE_DIST_MIRROR=https://mirrors.ustc.edu.cn/node/
