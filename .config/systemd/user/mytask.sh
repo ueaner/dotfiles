01f0962cbe92716ea4a3ab2e5105e7d8dd1b1aaa
ueaner <ueaner@gmail.com>
2024-12-21 02:19:27 +0800

    perf(mytask): check only during new releases of fedora

--- .config/systemd/user/mytask.sh
+++ .config/systemd/user/mytask.sh
@@ -30,6 +30,9 @@ function check-update-kernel-headers() {
     fi
 
     ignore_versions=(
+        # https://discussion.fedoraproject.org/t/broadcom-wl-not-working-after-upgrading-kernel-to-6-12-4/139948
+        # https://bugzilla.rpmfusion.org/show_bug.cgi?id=7130
+        # https://github.com/rpmfusion/wl-kmod/commit/16a67f7
         "kernel-headers-6.12.4-200.fc41"
     )
 
@@ -47,13 +50,18 @@ function check-update-kernel-headers() {
 ${latest_version}
 
 
-1. See https://src.fedoraproject.org/rpms/kernel-headers
-   and https://packages.fedoraproject.org/pkgs/kernel-headers/kernel-headers/
+1. See:
+
+    - https://src.fedoraproject.org/rpms/kernel-headers
+    - [Fedora Discussion]: https://discussion.fedoraproject.org/ #broadcom #wifi #f${VERSION_ID} #macbook
+    - [RPM Fusion Bugzilla]: https://bugzilla.rpmfusion.org/buglist.cgi?bug_status=all&component=broadcom-wl&component=wl-kmod&order=bug_id%20DESC&product=Fedora&version=f41&version=f40
+    - [RPM Fusion wl-kmod]: https://github.com/rpmfusion/wl-kmod
 
 2. Comment out the "exclude=kernel* kernel-*" line in the "/etc/dnf/dnf.conf" file.
 
 3. Upgrading Kernel:
 
+    sudo dnf upgrade --refresh
     sudo dnf update
     sudo akmods
     reboot
@@ -66,7 +74,31 @@ check-update-kernel-headers
 ### Check for updates to the Fedora release
 
 function check-update-fedora-release() {
+    # https://fedorapeople.org/groups/schedule/f-42/f-42-key-tasks.html
+    releases=(
+        f42 2025-04-22
+        f43 2025-11-11
+    )
+
     ver=$(($(awk '{print $3}' </etc/redhat-release) + 1))
+    check=false
+    release_date=
+
+    for ((i = 0; i < ${#releases[@]} / 2; i++)); do
+        fversion=${releases[i * 2 + 1]}
+        fdate=${releases[i * 2 + 2]}
+        if [[ "${fversion}" == "f${ver}" ]] && verlte "${fdate}" "$(date '+%Y-%m-%d')"; then
+            release_date=$fdate
+            check=true
+            break
+        fi
+    done
+
+    if ! $check; then
+        echo "Fedora $ver Non-stable, $release_date" >>/tmp/mytask.log
+        return
+    fi
+
     # ver=$(expr "$(awk '{print $3}' </etc/redhat-release)" + 1)
     httpcode=$(curl --silent --head "https://mirror.nyist.edu.cn/fedora/releases/$ver/" | awk '/^HTTP/{print $2}')
 
