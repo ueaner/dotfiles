e5d08ba1b5854cfe704b33e8f4086f85cbe038e4
ueaner <ueaner@gmail.com>
2025-08-14 06:15:32 +0800

    feat(hammerspoon): load EmmyLua spoon for Lua autocompletion support

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,32 +1,36 @@
 -- Set Hammerspoon's configuration file path to ~/.config/hammerspoon/init.lua:
---- defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
+-- [x] defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
 
 -- Release <Super>H for Tmux:
---- defaults write NSGlobalDomain NSUserKeyEquivalents -dict-add "Hide alacritty" "\0"
+-- [x] defaults write NSGlobalDomain NSUserKeyEquivalents -dict-add "Hide alacritty" nil
 
 -- Disable Spotlight indexing:
---- sudo mdutil -a -i off
+-- [x] sudo mdutil -a -i off
 
--- Modify Launchpad shortcut key to Super-Space:
---- System Preferences -> Keyboard -> Spotlight -> Disable all Spotlight shortcuts
---- System Preferences -> Keyboard -> Launchpad & Dock -> Change the Show Launchpad shortcut to ⌘Space
+-- Release <Super>Space for Launchpad:
+-- [x] System Preferences -> Keyboard -> Spotlight -> Disable all Spotlight shortcuts
+
+-- Toggle Launchpad: using System Preferences or Hammerspoon
+-- [ ] System Preferences -> Keyboard -> Launchpad & Dock -> Change the Show Launchpad shortcut to ⌘Space
 
 -- Toggle Launchpad
-hs.hotkey.bind({"cmd"}, "space", function()
-  hs.task.new("/usr/bin/open", nil, function() end, {"-a", "Launchpad"}):start()
-end)
-
--- Reload the Hammerspoon configuration
-hs.hotkey.bind({"cmd", "ctrl"}, "r", function()
-  hs.reload()
-  hs.notify.new({
-    title = "Hammerspoon",
-    informativeText = "Config reloaded!",
-    soundName = "Hero"
-  }):send()
+hs.hotkey.bind({ "cmd" }, "space", function()
+  hs.task.new("/usr/bin/open", nil, function() end, { "-a", "Launchpad" }):start()
 end)
 
 -- Toggle Alacritty terminal
 hs.hotkey.bind({ "cmd" }, "return", function()
   hs.application.launchOrFocus(os.getenv("HOME") .. "/Applications/Alacritty.app")
+end)
+
+-- Load EmmyLua spoon for Lua autocompletion support
+-- Download from: https://github.com/Hammerspoon/Spoons/raw/master/Spoons/EmmyLua.spoon.zip
+if hs.spoons.isInstalled("EmmyLua") ~= nil then
+  hs.loadSpoon("EmmyLua")
+end
+
+-- Reload the Hammerspoon configuration
+hs.hotkey.bind({ "cmd", "ctrl" }, "r", function()
+  hs.reload()
+  hs.notify.show("Hammerspoon", "Config reloaded!", "")
 end)
